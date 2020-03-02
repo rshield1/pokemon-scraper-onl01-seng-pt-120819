85adeaff0824d433b41c2515aafef82bae6f2ed9
Robert Shields <robert.shieldsiv@gmail.com>
2020-03-02 18:31:00 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,6 @@
 class Pokemon
-end
+  attr_accessor :id, :name, :type, :hp, :db
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
